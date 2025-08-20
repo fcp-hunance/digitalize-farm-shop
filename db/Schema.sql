bd577f0f31f8e16cb59788596ce8a9c970151ce7
Marten <Aboul.kataa@gmail.com>
2025-08-20 14:31:25 +0200

    Test

--- db/Schema.sql
+++ db/Schema.sql
@@ -136,6 +136,7 @@ CREATE TABLE IF NOT EXISTS `t_user` (
   `Username` varchar(30) NOT NULL,
   `PasswordHash` char(60) NOT NULL,
   `PIN` char(97) DEFAULT NULL,
+  'Role'varchar(30) NOT NULL,
   PRIMARY KEY (`UserID`),
   UNIQUE KEY `Username` (`Username`),
   UNIQUE KEY `PersonID` (`PersonID`),
