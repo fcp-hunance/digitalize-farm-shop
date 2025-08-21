dd67311ebda429ce9514934d5e7c265fe1bba669
Marten <Aboul.kataa@gmail.com>
2025-08-21 14:09:44 +0200

    Update

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,11 +1,7 @@
 const db = require('../db');
 
 const getAllProducts = async () => {
-<<<<<<< HEAD
-  const rows = await db.query("SELECT * FROM t_product"); 
-=======
-  const [rows] = await pool.query("SELECT * FROM t_Product");
->>>>>>> 8079393ae86cf0f1cf6ca356aaaaf10bf8853e4f
+  const rows = await db.query("SELECT * FROM t_Product"); 
   return rows;
 };
 
